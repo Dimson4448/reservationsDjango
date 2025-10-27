1991dab00135e371b8b38d1f025c6b674b972a53
Dimson4448 <tvd4468@gmail.com>
2025-10-27 19:04:13 +0100

    Frontend authentication - Login

--- reservations/settings.py
+++ reservations/settings.py
@@ -126,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentification
+LOGIN_URL = '/accounts/login'
+LOGIN_REDIRECT_URL = '/'
+
+

585b2c257471ce437ab5a58404aa101ba4343bbe
Dimson4448 <tvd4468@gmail.com>
2025-12-02 12:25:27 +0100

    View & template location with shows

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -1,5 +1,4 @@
-"""
-reservations.catalogue URL Configuration
+"""reservations.catalogue URL Configuration
 """
 from django.urls import path
 
@@ -13,15 +12,14 @@ urlpatterns = [
     path('artist/edit/<int:artist_id>', views.artist.edit, name='artist-edit'),
     path('artist/create', views.artist.create, name='artist-create'),
     path('artist/delete/<int:artist_id>', views.artist.delete, name='artist-delete'),
-
     path('type/', views.type.index, name='type-index'),
     path('type/<int:type_id>', views.type.show, name='type-show'),
-
     path('locality/', views.locality.index, name='locality-index'),
     path('locality/<int:locality_id>', views.locality.show, name='locality-show'),
-
     path('price/', views.price.index, name='price-index'),
     path('price/<int:price_id>', views.price.show, name='price-show'),
+    path('location/', views.location.index, name='location-index'),
+    path('location/<int:location_id>', views.location.show, name='location-show'),
 ]
 
 
